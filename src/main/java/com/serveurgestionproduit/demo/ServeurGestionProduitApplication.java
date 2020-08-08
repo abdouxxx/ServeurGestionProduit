c8c207eb4ccb57567336f2c4797321245570bb1f
abdoulaye ba <abdoulayebw@gmail.com>
2020-08-08 22:25:05 +0000

    first commit

---
+++ src/main/java/com/serveurgestionproduit/demo/ServeurGestionProduitApplication.java
@@ -0,0 +1,13 @@
+package com.serveurgestionproduit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionProduitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeurGestionProduitApplication.class, args);
+    }
+
+}
